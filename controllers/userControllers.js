d0525b11c4aa88354a34f2c92b5739f97cee0ff7
Ricky Aponte <Rickyapontephotography@gmail.com>
2023-01-25 11:22:19 -0500

    added some user controllers

---
+++ controllers/userControllers.js
@@ -0,0 +1,37 @@
+const {User} = require('../models/userModel');
+
+module.exports = {
+
+    // Get all users
+    getAllUsers: async (req, res) => {
+        try {
+            const users = await User.find();
+            res.status(200).json(users);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    // Get a single user
+    getSingleUser: async (req, res) => {
+        try {
+            const user = await User.findById(req.params.id)
+            .populate('thoughts');
+            res.status(200).json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }   
+    },
+
+    // Create a new user
+    createUser: async (req, res) => {
+        try {
+            const user = await User.create(req.body);
+            res.status(200).json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+// Update a user by its _id and return the updated user
+   
